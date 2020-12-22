075a636670068137ec8f0165392f919f580edd9a
mina.kamel <mka@hellofresh.com>
2020-12-22 23:59:03 +0100

    [Main] Loaded streaming fragment in MainActivity

--- app/src/main/java/com/minafkamel/musically/ui/main/MainActivity.kt
+++ app/src/main/java/com/minafkamel/musically/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import com.minafkamel.musically.R
 import com.minafkamel.musically.ui.artists.ArtistsFragment
 import com.minafkamel.musically.ui.base.BaseActivity
 import com.minafkamel.musically.ui.songs.SongsFragment
+import com.minafkamel.musically.ui.streaming.StreamingFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java, R.layout.a_main) {
@@ -17,6 +18,7 @@ class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java, R.la
         super.onCreate(savedInstanceState)
 
         loadArtistsFragment()
+        loadStreamingFragment()
     }
 
     private fun loadArtistsFragment() {
@@ -29,6 +31,12 @@ class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java, R.la
         supportFragmentManager.beginTransaction()
             .replace(R.id.frameLayout, SongsFragment.newInstance(permalink))
             .addToBackStack(SongsFragment.TAG)
+            .commit()
+    }
+
+    private fun loadStreamingFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frameLayoutStreaming, StreamingFragment.newInstance())
             .commit()
     }
 
